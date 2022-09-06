1aeb6bf90329fb111748f9ee9a0f2928bf7fb450
fwbrandao <fwbrandaobeta@gmail.com>
2022-09-06 13:03:04 +0100

    feat: sharing dependencies

--- ecomm/cart/src/index.js
+++ ecomm/cart/src/index.js
@@ -1,6 +1,1 @@
-import faker from 'faker';
-
-const messageCart = `<h1>You have ${faker.random.number()} items in you basket</h1>`;
-
-
-document.querySelector('#dev-cart').innerHTML = messageCart;
+import('./bootstrap');
