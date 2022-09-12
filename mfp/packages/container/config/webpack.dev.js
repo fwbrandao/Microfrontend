56ac078ec69a73ad6ceaad3deb4790da9de5be63
fwbrandao <fwbrandaobeta@gmail.com>
2022-09-12 08:36:24 +0100

    feat: sharing dependencies between container and child UIs

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -1,8 +1,8 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -20,7 +20,8 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-      }
+      },
+      shared: packageJson.dependencies,
     })
   ]
 };
